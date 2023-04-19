bb8388010a6d484fa86137326e86deff01d978ae
Leo B Jacob <leobjacob@gmail.com>
2023-04-19 10:36:04 -0400

    Update twilio

--- app/services/twilio_service.rb
+++ app/services/twilio_service.rb
@@ -1,7 +1,7 @@
 class TwilioService
     def initialize
-      @account_sid = ENV.fetch("TWILIO_ACCOUNT_SID")
-      @auth_token = ENV.fetch("TWILIO_AUTH_TOKEN")
+      @account_sid = 'AC024ae74f58d6020bd5be705bfdc8284f'
+      @auth_token = 'af13b08fae680b9998ef3611c0875213'
       @from = '+18445780397'
       @client = Twilio::REST::Client.new(@account_sid, @auth_token)
     end
